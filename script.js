0ebced7622d1e7707569af96846fba9a0fee4663
baig-mirza <90093947+baig-mirza@users.noreply.github.com>
2023-10-04 22:29:23 +1100

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ document.querySelector('.scroll-down-arrow').addEventListener('click', function 
     // Animate the reveal effect
     const landing = document.querySelector('.landing');
     const content = document.querySelector('.content');
+    const arrow = document.querySelector('.scroll-down-arrow');
 
     // Translate the landing and fade out the landing
     landing.style.transform = 'translateY(-100%)';
     landing.style.opacity = '0';
+    arrow.style.display = 'none';
 
     // After a short delay (adjust as needed), show the content
     setTimeout(() => {
