0b5736a8777adab6cc8d13b3ef757677e773cee5
baig-mirza <90093947+baig-mirza@users.noreply.github.com>
2023-10-12 19:36:26 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,104 @@
-// Function to enable scrolling when arrow is clicked
-document.querySelector('.scroll-down-arrow').addEventListener('click', function () {
+$(document).ready(function () {
+  $("#popup-button").click(function () {
+      $("#popup").fadeIn();
+  });
+
+  $("#close-popup").click(function () {
+      $("#popup").fadeOut();
+  });
+});
+
+
+$(document).ready(function () {
+  $("#collection-button").click(function () {
+      $("#popup").fadeIn();
+  });
+
+  $("#close-popup").click(function () {
+      $("#popup").fadeOut();
+  });
+});
+
+// Define your products with names, prices, and any other relevant details
+const products = [
+    { name: 'Lana Blue Hoodie', price: 79.99, stripeProductId: 'price_1NzW9FFBKA9GuLojiWMVcW46' },
+    // Add more products as needed
+  ];
+  
+  // Create an empty cart array
+  let cart = [];
+  
+  // Function to add an item to the cart
+  function addToCart(productName, price) {
+    cart.push({ name: productName, price: price });
+    updateCartUI();
+  }
+  
+  // Function to update the cart UI
+  function updateCartUI() {
+    // Get the cart items element by its ID
+    const cartItemsElement = document.getElementById('cart-items');
+  
+    // Clear the previous contents
+    cartItemsElement.innerHTML = '';
+  
+    // Iterate through the cart array and add each item to the cartItemsElement
+    for (const item of cart) {
+      // Create a new list item for the item
+      const listItem = document.createElement('li');
+      // Set the text content of the list item with the item name and price
+      listItem.textContent = `${item.name}: $${item.price.toFixed(2)}`;
+      // Append the list item to the cartItemsElement
+      cartItemsElement.appendChild(listItem);
+    }
+  
+    // Calculate and display the cart total
+    const cartTotal = cart.reduce((total, item) => total + item.price, 0);
+    document.getElementById('cart-total').textContent = cartTotal.toFixed(2);
+  }
+  
+  // Add a click event listener to the "Add to Cart" button
+  document.querySelectorAll('.addToCart').forEach(button => {
+    button.addEventListener('click', function () {
+      const productName = this.getAttribute('data-product-name');
+      const price = parseFloat(this.getAttribute('data-product-price'));
+      addToCart(productName, price);
+    });
+  });
+  
+  const checkoutButton = document.getElementById('checkout-btn');
+checkoutButton.addEventListener('click', checkout);
+  // Function to initiate the Stripe checkout
+// Function to initiate the Stripe checkout
+async function checkout() {
+    // Use the Stripe.js library to initiate checkout
+    const stripe = Stripe('pk_test_51NytAEFBKA9GuLojSNhZSIryOI6DGi1Or6NofwczqEtzjVNMiV1UyGU1LR4oxcJo70nvOeiDJa7m9DUtSgB0NAI900cNQu8tCD'); // Replace with your actual Stripe public key
+  
+    const lineItems = cart.map(item => ({
+      price: item.stripeProductId, // Use the stripeProductId here
+      quantity: 1,
+      currency: 'aud', // Replace with the appropriate currency code (e.g., 'aud')
+    }));
+  
+    const response = await fetch('/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        lineItems: lineItems, // Use "line_items" instead of "lineItems"
+      }),
+    });
+  
+    const { sessionId } = await response.json();
+  
+    // Redirect to Stripe checkout page
+    await stripe.redirectToCheckout({ sessionId: sessionId });
+  }
+  
+  // Add a click event listener to the "Checkout" button in the modal
+ 
+  document.querySelector('.scroll-down-arrow').addEventListener('click', function () {
     // Animate the reveal effect
     const landing = document.querySelector('.landing');
     const content = document.querySelector('.content');
@@ -23,25 +122,8 @@ document.querySelector('.scroll-down-arrow').addEventListener('click', function 
 
 
 // Rest of your code...
-$(document).ready(function () {
-    $("#popup-button").click(function () {
-        $("#popup").fadeIn();
-    });
 
-    $("#close-popup").click(function () {
-        $("#popup").fadeOut();
-    });
-});
 
-$(document).ready(function () {
-    $("#collection-button").click(function () {
-        $("#popup").fadeIn();
-    });
-
-    $("#close-popup").click(function () {
-        $("#popup").fadeOut();
-    });
-});
 
 
 var menu = document.querySelector(".mobile-menu")
